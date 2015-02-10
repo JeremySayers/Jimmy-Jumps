907bcb328efb3836221fd54c424377e6363ccc29
Jeremy Sayers <jsayers@student.dwc.edu>
2015-02-10 02:29:17 -0500

    Just for you Brian.
    Added tons of comments in the Player class. Optimized collisions, and
    fixed the logic a bit. Just more coders procrastination until I make the
    map scrollable.

--- Player.py
+++ Player.py
@@ -7,14 +7,19 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-
+    '''
+    Local Variables for movement, and jumping
+    '''
     is_moving_left = False
     is_moving_right = False
     player_mid_jump = False
     player_wall_jump = False
-    #player_double_jump = False
     player_x_speed = 5
-
+    
+    '''
+    Creates the player object, setting basic properties
+    and it's image.
+    '''
     def __init__(self, screen_width, screen_height):
         pygame.sprite.Sprite.__init__(self)
         self.width = 50
@@ -31,68 +36,95 @@ class Player(pygame.sprite.Sprite):
         self.rect.y = screen_height
         self.rect.x = 20
 
+    '''
+    Where most of the magic happens, more comments to come
+    '''
     def update(self, platform_list):
         self.platform_list = platform_list
         self.calc_gravity()
-
+        
+        '''
+        The first block is to see if the player should be moving, and if it should,
+        make sure that it's with in the screens bounds (Or maps in the future ^_^)
+        '''
         if self.is_moving_left and self.rect.x > 0:
             self.rect.x -= self.player_x_speed
         elif self.is_moving_right and self.rect.x < self.screen_width - self.width:
             self.rect.x += self.player_x_speed
-
-        block_hit_list = pygame.sprite.spritecollide(self, self.platform_list, False)
-        for block in block_hit_list:
-            # If we are moving right,
-            # set our right side to the left side of the item we hit
+        
+        '''
+        This next block checks to see if there is a platform colliding with the player
+        in terms of moving horizontally, and if so, places the player at the corresponding side of it
+        '''
+        platform_hit = pygame.sprite.spritecollideany(self, self.platform_list)
+        if platform_hit != None:
             if self.is_moving_right:
-                self.rect.x = block.rect.x - self.rect.width
+                self.rect.right = platform_hit.rect.left
             elif self.is_moving_left:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.x = block.rect.x + block.rect.width
-
+                self.rect.left = platform_hit.rect.right
+        
+        '''
+        Now we move the player vertically and then check to make sure we didn't collide with anything.
+        If we did, we just set the object to be above or below what it collided with depending on its'
+        y velocity
+        '''
         self.rect.y += self.y_vel
 
-        block_hit_list = pygame.sprite.spritecollide(self, self.platform_list, False)
-        for block in block_hit_list:
+        platform_hit = pygame.sprite.spritecollideany(self, self.platform_list)
+        if platform_hit != None:
             if self.y_vel > 0:
-                self.rect.y = block.rect.y - self.rect.height
+                self.rect.bottom = platform_hit.rect.top
             elif self.y_vel < 0:
-                self.rect.y = block.rect.y + block.rect.height
+                self.rect.top = platform_hit.rect.bottom
 
             # Stop our vertical movement
             self.y_vel = 0
 
     def calc_gravity(self):
+        '''
+        Moves the player's rect down 2 pixels incase we are working with a moveing platform,
+        then checks for collisions with platforms
+        '''
         self.rect.y += 2
-        platform_hit_list = pygame.sprite.spritecollide(self, self.platform_list, False)
+        platform_hit = pygame.sprite.spritecollideany(self, self.platform_list)
         self.rect.y -= 2
 
-        # See if we are on the ground.
-        if (self.rect.y >= self.screen_height - self.rect.height  and self.y_vel >= 0):
+        '''
+        The first if checks if we are on the ground or possibly 'in' the ground,
+        and if so it resets us to the ground height
+        '''
+        if (self.rect.bottom >= self.screen_height  and self.y_vel >= 0):
             self.y_vel = 0
             self.player_mid_jump = False
             self.player_wall_jump = False
-            #self.player_double_jump = False
-            self.rect.y = self.screen_height - self.rect.height
-        elif (len(platform_hit_list) > 0 and self.y_vel >= 0):
-            self.y_vel = 0
-            self.player_mid_jump = False
+            self.rect.bottom = self.screen_height
+        elif (platform_hit and self.y_vel >= 0):
+            self.y_vel = 0                                      #The else see's if we are instead hitting a platform from the top. If we are
+            self.player_mid_jump = False                        #it stops us and places us on top of the platform
             self.player_wall_jump = False
-            self.rect.y = platform_hit_list[0].rect.y - self.rect.height
+            self.rect.bottom = platform_hit.rect.top
         else:
-            self.y_vel += .35
+            self.y_vel += .35                                   #This last bit just increases our velocity if we arn't hitting anything, I feel like there's a word for it....
 
     def jump(self):
+        '''
+        Moves the player's rect down 2 pixels incase we are working with a moveing platform,
+        then checks for collisions with platforms
+        '''
         self.rect.y += 2
-        platform_hit_list = pygame.sprite.spritecollide(self, self.platform_list, False)
+        platform_hit = pygame.sprite.spritecollideany(self, self.platform_list)
         self.rect.y -= 2
 
-        # If it is ok to jump, set our speed upwards
-        if len(platform_hit_list) > 0 or self.rect.bottom >= self.screen_height:
+        '''
+        The first bit checks to see if we are on top of a platform or on the ground.
+        If we are then we start our jump! If not it checks to see if we are close enough to a wall
+        to perform a wall jump, if we are then once again we jump!
+        '''
+        if platform_hit or self.rect.bottom >= self.screen_height:
             if self.player_mid_jump == False:
                 self.y_vel = -11
                 self.player_mid_jump = True
-        elif (self.rect.x <= 5 and not self.player_wall_jump and self.is_moving_left) or (self.rect.x > self.screen_width - self.width -5 and not self.player_wall_jump and self.is_moving_right):
+        elif (self.rect.left <= 5 and not self.player_wall_jump) or (self.rect.right >= self.screen_width -5 and not self.player_wall_jump):
             self.y_vel = -11
             self.player_mid_jump = True
             self.player_wall_jump = True
