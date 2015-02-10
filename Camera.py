e0d472151b4a4d9194b22258d32acd42a526f4f2
Jeremy Sayers <jsayers@student.dwc.edu>
2015-02-10 18:07:48 -0500

    Better?

--- Camera.py
+++ Camera.py
@@ -16,44 +16,48 @@ class Camera(object):
         self.map_height = map_height
         
     def update_player(self, player_sprite_group):
-        player_sprite_group_render = player_sprite_group.copy()
-        for player in player_sprite_group_render:
+        player_sprite_group_render = pygame.sprite.Group()
+        for player in player_sprite_group:
+            player_render = Player(self.map_width, self.map_height)
+            player_sprite_group_render.add(player_render)
             
             if player.rect.centerx < self.screen_width/2:
-                player.rect.centerx = player.rect.centerx
+                player_render.rect.centerx = player.rect.centerx
             elif player.rect.centerx > self.map_width - (self.screen_width/2):
-                player.rect.centerx = (self.screen_width/2) + (player.rect.centerx - (self.map_width - (self.screen_width/2)))
+                player_render.rect.centerx = (self.screen_width/2) + (player.rect.centerx - (self.map_width - (self.screen_width/2)))
             else:
-                player.centerx = self.screen_width/2
+                player_render.centerx = self.screen_width/2
                 
                 
             if player.rect.centery < self.screen_height/2:
-                player.rect.centery = player.rect.centery
+                player_render.rect.centery = player.rect.centery
             elif player.rect.centery > self.map_height - (self.screen_height/2):
-                player.rect.centery = (self.screen_height/2) + (player.rect.centery - (self.map_height - (self.screen_height/2)))
+                player_render.rect.centery = (self.screen_height/2) + (player.rect.centery - (self.map_height - (self.screen_height/2)))
             else:
-                player.centery = self.screen_height/2
+                player_render.centery = self.screen_height/2
             
         return player_sprite_group_render
         
         
     def update_platforms(self, platform_list, player):
-        platform_list = platform_list.copy()
+        platform_list_render = pygame.sprite.Group()
             
         for platform in platform_list:
+            platform_render = Platform(platform.rect.width, platform.rect.height, platform.rect.x, platform.rect.y)
+            platform_list_render.add(platform_render)
             if (platform.rect.left > player.rect.centerx - self.screen_width/2) and (platform.rect.right < player.rect.centerx + self.screen_width/2):
                 if (platform.rect.bottom > player.rect.centery - self.screen_height/2) and (platform.rect.top < player.rect.centery + self.screen_height/2):
                     if platform.rect.centerx < player.rect.centerx:
-                        platform.rect.centerx = player.rect.centerx - platform.rect.centerx
+                        platform_render.rect.centerx = player.rect.centerx - platform.rect.centerx
                     elif platform.rect.centerx > player.rect.centerx:
-                        platform.rect.centerx = (platform.rect.centerx - player.rect.centerx) + player.rect.centerx
+                        platform_render.rect.centerx = (platform.rect.centerx - player.rect.centerx) + player.rect.centerx
                     
                     if platform.rect.centery < player.rect.centery:
-                        platform.rect.centery = player.rect.centery - platform.rect.centery
+                        platform_render.rect.centery = player.rect.centery - platform.rect.centery
                     elif platform.rect.centery > player.rect.centery:
-                        platform.rect.centery = (platform.rect.centery - player.rect.centery) + player.rect.centery
+                        platform_render.rect.centery = (platform.rect.centery - player.rect.centery) + player.rect.centery
         
-        return platform_list
+        return platform_list_render
                         
                         
                         
