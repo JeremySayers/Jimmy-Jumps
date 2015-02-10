17210c538d4228644bfa97989ef950b75062ab9d
Jeremy Sayers <jsayers@student.dwc.edu>
2015-02-10 17:45:13 -0500

    Changed some stuff in the camera. Still not working.

--- Camera.py
+++ Camera.py
@@ -16,44 +16,42 @@ class Camera(object):
         self.map_height = map_height
         
     def update_player(self, player_sprite_group):
-        self.player_sprite_group = player_sprite_group
-        for self.player in self.player_sprite_group:
+        player_sprite_group_render = player_sprite_group.copy()
+        for player in player_sprite_group_render:
             
-            if self.player.rect.centerx < self.screen_width/2:
-                self.player.rect.centerx = self.player.rect.centerx
-            elif self.player.rect.centerx > self.map_width - (self.screen_width/2):
-                self.player.rect.centerx = (self.screen_width/2) + (self.player.rect.centerx - (self.map_width - (self.screen_width/2)))
+            if player.rect.centerx < self.screen_width/2:
+                player.rect.centerx = player.rect.centerx
+            elif player.rect.centerx > self.map_width - (self.screen_width/2):
+                player.rect.centerx = (self.screen_width/2) + (player.rect.centerx - (self.map_width - (self.screen_width/2)))
             else:
-                self.player.centerx = self.screen_width/2
+                player.centerx = self.screen_width/2
                 
                 
-            if self.player.rect.centery < self.screen_height/2:
-                self.player.rect.centery = self.player.rect.centery
-            elif self.player.rect.centery > self.map_height - (self.screen_height/2):
-                self.player.rect.centery = (self.screen_height/2) + (self.player.rect.centery - (self.map_height - (self.screen_height/2)))
+            if player.rect.centery < self.screen_height/2:
+                player.rect.centery = player.rect.centery
+            elif player.rect.centery > self.map_height - (self.screen_height/2):
+                player.rect.centery = (self.screen_height/2) + (player.rect.centery - (self.map_height - (self.screen_height/2)))
             else:
-                self.player.centery = self.screen_height/2
+                player.centery = self.screen_height/2
             
-        return (self.player_sprite_group)
+        return player_sprite_group_render
         
         
     def update_platforms(self, platform_list, player):
-        self.player = player
-        self.platform_list = platform_list
-        self.platform_list_render = platform_list
+        platform_list = platform_list.copy()
             
-        for platform in self.platform_list:
-            if (platform.rect.left > self.player.rect.centerx - self.screen_width/2) and (platform.rect.right < self.player.rect.centerx + self.screen_width/2):
-                if (platform.rect.bottom > self.player.rect.centery - self.screen_height/2) and (platform.rect.top < self.player.rect.centery + self.screen_height/2):
-                    if platform.rect.centerx < self.player.rect.centerx:
-                        platform.rect.centerx = self.player.rect.centerx - platform.rect.centerx
-                    elif platform.rect.centerx > self.player.rect.centerx:
-                        platform.rect.centerx = (platform.rect.centerx - self.player.rect.centerx) + self.player.rect.centerx
+        for platform in platform_list:
+            if (platform.rect.left > player.rect.centerx - self.screen_width/2) and (platform.rect.right < player.rect.centerx + self.screen_width/2):
+                if (platform.rect.bottom > player.rect.centery - self.screen_height/2) and (platform.rect.top < player.rect.centery + self.screen_height/2):
+                    if platform.rect.centerx < player.rect.centerx:
+                        platform.rect.centerx = player.rect.centerx - platform.rect.centerx
+                    elif platform.rect.centerx > player.rect.centerx:
+                        platform.rect.centerx = (platform.rect.centerx - player.rect.centerx) + player.rect.centerx
                     
-                    if platform.rect.centery < self.player.rect.centery:
-                        platform.rect.centery = self.player.rect.centery - platform.rect.centery
-                    elif platform.rect.centery > self.player.rect.centery:
-                        platform.rect.centery = (platform.rect.centery - self.player.rect.centery) + self.player.rect.centery
+                    if platform.rect.centery < player.rect.centery:
+                        platform.rect.centery = player.rect.centery - platform.rect.centery
+                    elif platform.rect.centery > player.rect.centery:
+                        platform.rect.centery = (platform.rect.centery - player.rect.centery) + player.rect.centery
         
         return platform_list
                         
